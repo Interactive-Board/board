058b8220f2e93c418344d4eee88fa5a3f010c2b2
nicolas-lum <nicolas.lum@icloud.com>
2018-11-20 13:09:21 -1000

    added table for publication to database

--- database/ib-db.sql
+++ database/ib-db.sql
@@ -9,22 +9,44 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`board` /*!40100 DEFAULT CHARACTER SET l
 
 USE `board`;
 
-/*Table structure for table `customers` */
+/*Table structure for table `user` */
 
 DROP TABLE IF EXISTS `user`;
 
 CREATE TABLE `user` (
-  `userNumber` int(11) NOT NULL,
-  `userLastName` varchar(50) NOT NULL,
-  `userFirstName` varchar(50) NOT NULL,
-  `phone` varchar(50) NOT NULL,
-  `city` varchar(50) NOT NULL,
-  `state` varchar(50) DEFAULT NULL,
-  `country` varchar(50) NOT NULL,
-  PRIMARY KEY (`userNumber`),
-  CONSTRAINT `user_ibfk_1`  REFERENCES `user` (`userNumber`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+	`userNumber` int(11) NOT NULL,
+	`userLastName` varchar(50) NOT NULL,
+	`userFirstName` varchar(50) NOT NULL,
+	`phone` varchar(50) NOT NULL,
+	`city` varchar(50) NOT NULL,
+	`state` varchar(50) DEFAULT NULL,
+	`country` varchar(50) NOT NULL,
+	PRIMARY KEY (`userNumber`),
+	CONSTRAINT `user_ibfk_1`  REFERENCES `user` (`userNumber`)
+	)
+ENGINE=InnoDB DEFAULT CHARSET=utf-8;
 
 insert  into `user`(`userNumber`,`userLastName`,`userFirstName`,`phone`,`city`,`state`,`country`) values 
 
-DROP TABLE IF EXISTS
+
+/* Table structure for table publication*/
+DROP TABLE IF EXISTS `publication`;
+
+CREATE TABLE `publication`(
+	`pubilicationNumber` int(11) NOT NULL,
+	`title` varchar(50) NOT NULL,
+	`author1` varchar(50) NOT NULL,
+	`author2` varchar(50) NOT NULL,
+	`abstract` varchar(50) NOT NULL,
+	`imageReference` varchar(50) NOT NULL,
+	`url` varchar(50) NOT NULL,
+	PRIMARY KEY (`publicationNumber`),
+	CONSTRAINT `publication_ibfk_` REFERENCES `user` (`userNumber`)
+	)
+ENGINE=InnoDB DEFAULT CHARSET=utf-8;
+
+insert into `publication`(`publicationNumber`,`title`,`author1`,`author2`,`abstract`imageReference`,`url`)
+values
+
+
+	
