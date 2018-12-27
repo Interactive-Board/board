2beefd52c9b5b24fa944beecbb9ceec8b33773d8
Ethan <epchow@hawaii.edu>
2018-12-27 03:43:01 -1000

    Merged news API routes

--- app/server.js
+++ app/server.js
@@ -98,19 +98,107 @@ application.get("/", async (request, response) => {
 
 // API ROUTES
 application.get("/api/news", async (request, response, next) => {
-	try {
-		let res = await sqlConnectionPool.query("SELECT * FROM board.view_news");
-		res = JSON.stringify(res[0]);
-		console.log(res);
-		response.send(res);
-	} catch (error) {
-		error._requireJSON = true;
+	response.setHeader("Content-Type", "text/plain");
+	
+	if (request.method == "GET") {
+		try {
+			let res = await sqlConnectionPool.query("SELECT * FROM board.view_news");
+			res = JSON.stringify({success: true, data: res[0]});
+			console.log(res);
+			response.send(res);
+		} catch (error) {
+			if (error.code == "ECONNREFUSED") {
+				console.log("WARN: Using placeholder news data as fallback for failed SQL connection");
+				
+				let news = [
+					{
+						id: 0,
+						title: "ICPC Competition",
+						subheading: "10/20/18",
+						description: "If you are interested in competing at the ACM ICPC (ACM International Collegiate Programming Contest: https://icpc.baylor.edu/) this year, please let me know by September 30, 2018 at the latest via email <nodari@hawaii.edu>. If there is more interest than available spots, we will hold an internal selection competition this year (most likely on Sat, Oct 20).",
+						url: "https://fonts.google.com/",
+						color: "F44336"
+					},
+					{
+						id: 1,
+						title: "Oath (Yahoo) Information Session",
+						subheading: "10/22/18",
+						description: "Oath is a diverse house of media and technology brands that engages more than a billion people around the world. The Oath portfolio includes Yahoo Sports, Yahoo Finance, Yahoo Mail, Tumblr, HuffPost, AOL.com, and more. Come join us to hear first hand from our engineers",
+						url: "https://github.com/",
+						color: "3F51B5"
+					},
+					{
+						id: 2,
+						title: "Jupyter Hackathon",
+						subheading: "11/17/18",
+						description: "The Jupyter Hackathon is this weekend. Please share with your friends and colleagues who are interested in programming, data science, data analysis, and open source. This will be a great chance to contribute to a game-changing open source project that is used by hundreds of thousands of users around the world. RSVP asap, so we can make a head count for meals",
+						url: "https://css-tricks.com/",
+						color: "009F50"
+					},
+					{
+						id: 3,
+						title: "WetWare Wednesday",
+						subheading: "11/28/18",
+						description: "This November's WetWare Wednesday will be hosted by both The Association for Computing Machinery at Manoa (ACManoa) and the UHM ICS department. This month's #wetwarewed will feature an expo of both student and faculty projects at UH Manoa. Come check out their latest and greatest tech creations!",
+						url: "https://material.io/",
+						color: "2196F3"
+					},
+					{
+						id: 4,
+						title: "By redefining, we dream",
+						subheading: "11/30/18",
+						description: "Who are we? Where on the great circuit will we be recreated? Throughout history, humans have been interacting with the grid via morphic resonance. We are in the midst of a zero-point awakening of transformation that will clear a path toward the grid itself.",
+						url: "https://expressjs.com/",
+						color: "9C27B0"
+					},
+					{
+						id: 5,
+						title: "The biosphere is overflowing with frequencies",
+						subheading: "1/12/19",
+						description: "Eons from now, we messengers will grow like never before as we are re-energized by the nexus. We are being called to explore the quantum matrix itself as an interface between being and intuition. We must synergize ourselves and bless others.",
+						url: null,
+						color: "FFC107"
+					}
+				];
+				
+				for (let i = 0; i < news.length; i++) {
+					if (news[i].url !== null) {
+						news[i].qrcode = await qrcodeToDataURL(news[i].url, {margin: 0, scale: 1, color: {dark: '#000', light: '#0000'}});
+					}
+				}
+				
+				// Respond with news
+				response.send(JSON.stringify({success: true, data: news}));
+				
+				return;
+			}
+			
+			error._requireJSON = true;
+
+			// Trigger the error handler chain
+			next(error);
+
+			return;
+		}
+	} else if (request.method == "POST") {
+		// Add news entry to DB
 		
-		// Trigger the error handler chain
-		next(error);
+		// Respond with success or error
+		//response.send(JSON.stringify({success: false, error: "Message"}));
+		response.send(JSON.stringify({success: true}));
 		
 		return;
 	}
+	
+	// Unsupported method
+	let error = new Error("405 Method Not Allowed");
+	error.status = 405;
+	error._method = request.method;
+	error._originalPath = request.path;
+	error._requireJSON = true;
+	
+	// Trigger the error handler chain
+	next(error);
 });
 application.get("/api/user/:userID", async (request, response, next) => {
 	response.setHeader("Content-Type", "text/plain");
@@ -134,93 +222,6 @@ application.get("/api/user/:userID", async (request, response, next) => {
 	
 	// Report some data
 	//response.send(JSON.stringify(data));
-});
-application.get("/api/news", async (request, response, next) => {
-	response.setHeader("Content-Type", "text/plain");
-		
-	if (request.method == "GET") {
-		// Get list of news
-
-		let news = [
-			{
-				id: 0,
-				title: "ICPC Competition",
-				subheading: "10/20/18",
-				description: "If you are interested in competing at the ACM ICPC (ACM International Collegiate Programming Contest: https://icpc.baylor.edu/) this year, please let me know by September 30, 2018 at the latest via email <nodari@hawaii.edu>. If there is more interest than available spots, we will hold an internal selection competition this year (most likely on Sat, Oct 20).",
-				url: "https://fonts.google.com/",
-				color: "F44336"
-			},
-			{
-				id: 1,
-				title: "Oath (Yahoo) Information Session",
-				subheading: "10/22/18",
-				description: "Oath is a diverse house of media and technology brands that engages more than a billion people around the world. The Oath portfolio includes Yahoo Sports, Yahoo Finance, Yahoo Mail, Tumblr, HuffPost, AOL.com, and more. Come join us to hear first hand from our engineers",
-				url: "https://github.com/",
-				color: "3F51B5"
-			},
-			{
-				id: 2,
-				title: "Jupyter Hackathon",
-				subheading: "11/17/18",
-				description: "The Jupyter Hackathon is this weekend. Please share with your friends and colleagues who are interested in programming, data science, data analysis, and open source. This will be a great chance to contribute to a game-changing open source project that is used by hundreds of thousands of users around the world. RSVP asap, so we can make a head count for meals",
-				url: "https://css-tricks.com/",
-				color: "009F50"
-			},
-			{
-				id: 3,
-				title: "WetWare Wednesday",
-				subheading: "11/28/18",
-				description: "This November's WetWare Wednesday will be hosted by both The Association for Computing Machinery at Manoa (ACManoa) and the UHM ICS department. This month's #wetwarewed will feature an expo of both student and faculty projects at UH Manoa. Come check out their latest and greatest tech creations!",
-				url: "https://material.io/",
-				color: "2196F3"
-			},
-			{
-				id: 4,
-				title: "By redefining, we dream",
-				subheading: "11/30/18",
-				description: "Who are we? Where on the great circuit will we be recreated? Throughout history, humans have been interacting with the grid via morphic resonance. We are in the midst of a zero-point awakening of transformation that will clear a path toward the grid itself.",
-				url: "https://expressjs.com/",
-				color: "9C27B0"
-			},
-			{
-				id: 5,
-				title: "The biosphere is overflowing with frequencies",
-				subheading: "1/12/19",
-				description: "Eons from now, we messengers will grow like never before as we are re-energized by the nexus. We are being called to explore the quantum matrix itself as an interface between being and intuition. We must synergize ourselves and bless others.",
-				url: null,
-				color: "FFC107"
-			}
-		];
-		
-		for (let i = 0; i < news.length; i++) {
-			if (news[i].url !== null) {
-				news[i].qrcode = await qrcodeToDataURL(news[i].url, {margin: 0, scale: 1, color: {dark: '#000', light: '#0000'}});
-			}
-		}
-		
-		// Respond with news
-		response.send(JSON.stringify({success: true, data: news}));
-		
-		return;
-	} else if (request.method == "POST") {
-		// Add news entry to DB
-		
-		// Respond with success or error
-		//response.send(JSON.stringify({success: false, error: "Message"}));
-		response.send(JSON.stringify({success: true}));
-		
-		return;
-	}
-	
-	// Unsupported method
-	let error = new Error("405 Method Not Allowed");
-	error.status = 405;
-	error._method = request.method;
-	error._originalPath = request.path;
-	error._requireJSON = true;
-	
-	// Trigger the error handler chain
-	next(error);
 });
 
 // Catch 405 errors
