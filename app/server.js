41ef4255248e6032342c7cc231559bf6cd6e81cd
Ethan <epchow@hawaii.edu>
2018-12-27 05:16:24 -1000

    Fixed idle slideshow mockup to work with database property names

--- app/server.js
+++ app/server.js
@@ -112,58 +112,58 @@ application.get("/api/news", async (request, response, next) => {
 				
 				let news = [
 					{
-						id: 0,
-						title: "ICPC Competition",
-						subheading: "10/20/18",
-						description: "If you are interested in competing at the ACM ICPC (ACM International Collegiate Programming Contest: https://icpc.baylor.edu/) this year, please let me know by September 30, 2018 at the latest via email <nodari@hawaii.edu>. If there is more interest than available spots, we will hold an internal selection competition this year (most likely on Sat, Oct 20).",
-						url: "https://fonts.google.com/",
-						color: "F44336"
+						NID: 0,
+						NewsTitle: "ICPC Competition",
+						NewsSubheading: "10/20/18",
+						NewsDescription: "If you are interested in competing at the ACM ICPC (ACM International Collegiate Programming Contest: https://icpc.baylor.edu/) this year, please let me know by September 30, 2018 at the latest via email <nodari@hawaii.edu>. If there is more interest than available spots, we will hold an internal selection competition this year (most likely on Sat, Oct 20).",
+						NewsURL: "https://fonts.google.com/",
+						NewsImageColor: "F44336"
 					},
 					{
-						id: 1,
-						title: "Oath (Yahoo) Information Session",
-						subheading: "10/22/18",
-						description: "Oath is a diverse house of media and technology brands that engages more than a billion people around the world. The Oath portfolio includes Yahoo Sports, Yahoo Finance, Yahoo Mail, Tumblr, HuffPost, AOL.com, and more. Come join us to hear first hand from our engineers",
-						url: "https://github.com/",
-						color: "3F51B5"
+						NID: 1,
+						NewsTitle: "Oath (Yahoo) Information Session",
+						NewsSubheading: "10/22/18",
+						NewsDescription: "Oath is a diverse house of media and technology brands that engages more than a billion people around the world. The Oath portfolio includes Yahoo Sports, Yahoo Finance, Yahoo Mail, Tumblr, HuffPost, AOL.com, and more. Come join us to hear first hand from our engineers",
+						NewsURL: "https://github.com/",
+						NewsImageColor: "3F51B5"
 					},
 					{
-						id: 2,
-						title: "Jupyter Hackathon",
-						subheading: "11/17/18",
-						description: "The Jupyter Hackathon is this weekend. Please share with your friends and colleagues who are interested in programming, data science, data analysis, and open source. This will be a great chance to contribute to a game-changing open source project that is used by hundreds of thousands of users around the world. RSVP asap, so we can make a head count for meals",
-						url: "https://css-tricks.com/",
-						color: "009F50"
+						NID: 2,
+						NewsTitle: "Jupyter Hackathon",
+						NewsSubheading: "11/17/18",
+						NewsDescription: "The Jupyter Hackathon is this weekend. Please share with your friends and colleagues who are interested in programming, data science, data analysis, and open source. This will be a great chance to contribute to a game-changing open source project that is used by hundreds of thousands of users around the world. RSVP asap, so we can make a head count for meals",
+						NewsURL: "https://css-tricks.com/",
+						NewsImageColor: "009F50"
 					},
 					{
-						id: 3,
-						title: "WetWare Wednesday",
-						subheading: "11/28/18",
-						description: "This November's WetWare Wednesday will be hosted by both The Association for Computing Machinery at Manoa (ACManoa) and the UHM ICS department. This month's #wetwarewed will feature an expo of both student and faculty projects at UH Manoa. Come check out their latest and greatest tech creations!",
-						url: "https://material.io/",
-						color: "2196F3"
+						NID: 3,
+						NewsTitle: "WetWare Wednesday",
+						NewsSubheading: "11/28/18",
+						NewsDescription: "This November's WetWare Wednesday will be hosted by both The Association for Computing Machinery at Manoa (ACManoa) and the UHM ICS department. This month's #wetwarewed will feature an expo of both student and faculty projects at UH Manoa. Come check out their latest and greatest tech creations!",
+						NewsURL: "https://material.io/",
+						NewsImageColor: "2196F3"
 					},
 					{
-						id: 4,
-						title: "By redefining, we dream",
-						subheading: "11/30/18",
-						description: "Who are we? Where on the great circuit will we be recreated? Throughout history, humans have been interacting with the grid via morphic resonance. We are in the midst of a zero-point awakening of transformation that will clear a path toward the grid itself.",
-						url: "https://expressjs.com/",
-						color: "9C27B0"
+						NID: 4,
+						NewsTitle: "By redefining, we dream",
+						NewsSubheading: "11/30/18",
+						NewsDescription: "Who are we? Where on the great circuit will we be recreated? Throughout history, humans have been interacting with the grid via morphic resonance. We are in the midst of a zero-point awakening of transformation that will clear a path toward the grid itself.",
+						NewsURL: "https://expressjs.com/",
+						NewsImageColor: "9C27B0"
 					},
 					{
-						id: 5,
-						title: "The biosphere is overflowing with frequencies",
-						subheading: "1/12/19",
-						description: "Eons from now, we messengers will grow like never before as we are re-energized by the nexus. We are being called to explore the quantum matrix itself as an interface between being and intuition. We must synergize ourselves and bless others.",
-						url: null,
-						color: "FFC107"
+						NID: 5,
+						NewsTitle: "The biosphere is overflowing with frequencies",
+						NewsSubheading: "1/12/19",
+						NewsDescription: "Eons from now, we messengers will grow like never before as we are re-energized by the nexus. We are being called to explore the quantum matrix itself as an interface between being and intuition. We must synergize ourselves and bless others.",
+						NewsURL: null,
+						NewsImageColor: "FFC107"
 					}
 				];
 				
 				for (let i = 0; i < news.length; i++) {
-					if (news[i].url !== null) {
-						news[i].qrcode = await qrcodeToDataURL(news[i].url, {margin: 0, scale: 1, color: {dark: '#000', light: '#0000'}});
+					if (news[i].NewsURL !== null) {
+						news[i].QRCode = await qrcodeToDataURL(news[i].NewsURL, {margin: 0, scale: 1, color: {dark: '#000', light: '#0000'}});
 					}
 				}
 				
