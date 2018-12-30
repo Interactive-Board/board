8b2b86b6d58259382d41afa2cfe79704b92269fe
Ethan <epchow@hawaii.edu>
2018-12-29 16:20:18 -1000

    Created web components for more elements, added top bar mockup

---
+++ app/static/script/component/Button.js
@@ -0,0 +1,85 @@
+class Button extends HTMLElement {
+	constructor() {
+		// Always call super first in constructor
+		super();
+
+		// Create a shadow root
+		let shadow = this.attachShadow({mode: "open"});
+
+		// Create elements
+		let wrapper = document.createElement("button");
+		
+		this.text = "";
+		this._wrapperElement = wrapper;
+
+		let style = document.createElement("style");
+		style.textContent = `
+button {
+	min-width: 64px;
+	height: 36px;
+	margin: 0 5px;
+	padding: 0 16px;
+	border: 1px solid #DADCE0;
+	border-radius: 4px;
+	outline: none;
+	background: #FFF;
+	font-family: "Roboto", Helvetica, "Times New Roman", sans-serif;
+	font-display: swap;
+	font-size: 0.875em;
+	font-weight: bold;
+	line-height: 36px;
+	text-transform: uppercase;
+	box-sizing: border-box;
+	transition: border-color 50ms, background 50ms, box-shadow 250ms;
+	cursor: pointer;
+}
+
+button:first-child {
+	margin-left: 0;
+}
+
+button:last-child {
+	margin-right: 0;
+}
+
+button:hover {
+	box-shadow: 0 2px 4px -1px rgba(0,0,0,.2), 0 4px 5px 0 rgba(0,0,0,.08), 0 1px 10px 0 rgba(0,0,0,.12);
+}
+
+button:focus {
+	border-color: #2196F3;
+	opacity: 0.78;
+}
+`;
+
+		// Attach the created elements to the shadow dom
+		shadow.appendChild(style);
+		shadow.appendChild(wrapper);
+		
+		let observer = new MutationObserver((records, observer) => this.observed(records, observer));
+		let observationConfig = {attributeFilter: ["text"], attributes: true};
+		observer.observe(this, observationConfig);
+		
+		this.update();
+	}
+
+	connectedCallback() {
+		this.update();
+	}
+	
+	update() {
+		let text = this.getAttribute("text");
+
+		this._wrapperElement.innerText = text;
+	}
+	
+	observed(records, observer) {
+		for (let i = 0; i < records.length; i++) {
+			let record = records[i];
+
+			this.update();
+		}
+	}
+}
+
+customElements.define("button-element", Button);
---
+++ app/static/script/component/Container.js
@@ -0,0 +1,83 @@
+class Container extends HTMLElement {
+	constructor() {
+		// Always call super first in constructor
+		super();
+		
+		this._flow = false;
+		this._flex = null;
+
+		// Create a shadow root
+		let shadow = this.attachShadow({mode: "open"});
+		this._shadowElement = shadow;
+
+		// Create elements
+//		let wrapper = document.createElement("div");
+//		this._wrapperElement = wrapper;
+
+		let style = document.createElement("style");
+		style.textContent = `
+:host {
+	display: block;
+	overflow: auto;
+}
+
+:host([flow]) {
+	flex: 1;
+}
+
+:host([flex=row]) {
+	display: flex;
+	flex-direction: row;
+}
+
+:host([flex=column]) {
+	display: flex;
+	flex-direction: column;
+}
+`;
+
+		// Attach the created elements to the shadow dom
+		shadow.appendChild(style);
+//		shadow.appendChild(wrapper);
+		
+		let observer = new MutationObserver((records, observer) => this.observed(records, observer));
+		let observationConfig = {attributeFilter: ["flow", "flex"], attributes: true, childList: true};
+		observer.observe(this, observationConfig);
+		
+		this.update();
+	}
+
+	connectedCallback() {
+		this.update();
+	}
+	
+	update() {
+		let flow = this.getAttribute("flow") !== null;
+		let flex = this.getAttribute("flex");
+		
+		if (flex !== "row" && flex !== "column") {
+			flex = null;
+		}
+		
+		this._flow = flow;
+		this._flex = flex;
+				
+		while (this._shadowElement.children.length > 1) {
+			this._shadowElement.removeChild(this._shadowElement.lastChild);
+		}
+		
+		for (let i = 0; i < this.children.length; i++) {
+			this._shadowElement.appendChild(this.children[i].cloneNode(true));
+		}
+	}
+	
+	observed(records, observer) {
+		for (let i = 0; i < records.length; i++) {
+			let record = records[i];
+
+			this.update();
+		}
+	}
+}
+
+customElements.define("container-element", Container);
